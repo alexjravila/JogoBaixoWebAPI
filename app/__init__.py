9a214820212d336e2ba927fdaf5e73ec9af2ec39
Alex Junior Avila <alex_avila11@hotmail.com>
2019-06-01 20:00:39 -0300

    Setuping pytest and creating unit tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from connexion.resolver import RestyResolver
 import connexion
 
-def create_app():
+def create_app(test_config=None):
     app = connexion.FlaskApp(__name__, specification_dir='swagger/')
     app.add_api('swagger.yml')
 
